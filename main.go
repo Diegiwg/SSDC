bc8808f5105b6617e5c757cd447f10e3d83e7793
Diegiwg <diegiwg@gmail.com>
2024-02-17 15:25:59 -0300

    feat: Added the LOCAL command to run tests with data saved locally
    1. The LOCAL command will be used mainly for development, avoiding making API requests
    
    2. The API folder contains the data downloaded on 02/17/2024, with the filter of the 'fields' necessary for testing

--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package main
 
-import "github.com/Diegiwg/cli"
+import (
+	"errors"
+	"strconv"
+
+	"github.com/Diegiwg/cli"
+)
+
+var G_SAVE_FILES = false
+
+func RunCMD(ctx *cli.Context, callback func(int) error) error {
+	if len(ctx.Args) < 1 {
+		return errors.New("the distance to be covered is not provided")
+	}
+
+	distance, err := strconv.Atoi(ctx.Args[0])
+	if err != nil {
+		return errors.New("the distance to be covered is not a integer number")
+	}
+
+	if _, ok := ctx.Flags["save"]; ok {
+		G_SAVE_FILES = true
+	}
+
+	callback(distance)
+
+	return nil
+}
+
+func LocalCMD(ctx *cli.Context) error {
+	return RunCMD(ctx, func(i int) error {
+		// TODO: IMPL the local data handler
+		return nil
+	})
+}
 
 func main() {
 	app := cli.NewApp()
+
+	app.AddCommand(&cli.Command{
+		Name:  "local",
+		Desc:  "Test the application using the data saved in the API folder",
+		Help:  "Test the application using the data saved in the API folder",
+		Usage: "<distance: int as MGLT> [--save: optional]",
+		Exec:  LocalCMD,
+	})
 
 	err := app.Run()
 	if err != nil {
